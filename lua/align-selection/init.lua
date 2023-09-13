7ef6f97d503776bc0891d99a61c58511f203bc31
Chuck Flowers <t_chuck_flowers@yahoo.com>
2023-09-13 09:24:24 -0400

    Initial commit

---
+++ lua/align-selection/init.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+---@alias SelectionInfo { top: integer, bottom: integer, height: integer, center: integer }
+---@alias WindowInfo { width: integer, height: integer }
+
+--- @return nil
+function M.center()
+	local selection_info = getSelectionInfo()
+
+	vim.fn.feedkeys('<ESC>' .. selection_info.center .. 'Gzzgv')
+end
+
+--- @return WindowInfo
+local function getWindowInfo()
+	local winid = vim.fn.win_getid()
+	local width = vim.fn.winwidth(winid)
+	local height = vim.fn.winheight(winid)
+
+	return {
+		width = width,
+		height = height
+	}
+end
+
+--- @return SelectionInfo
+local function getSelectionInfo()
+	local top = vim.fn.line('v')
+	local bottom = vim.fn.line('.')
+
+	return {
+		top = top,
+		bottom = bottom,
+		height = bottom - top,
+		center = (top + bottom) / 2
+	}
+end
+
+return M
